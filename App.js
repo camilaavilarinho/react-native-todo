c09e57d0e6411991f05c0e8c33a721297e73091e
Camila Vilarinho <camilaavilarinho@gmail.com>
2019-11-23 16:52:13 +0000

    First commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Main from './app/Main'
+
+
+export default class App extends React.Component {
+  render(){
+    return <Main />
+  }
+}
+
+/* const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}); */
